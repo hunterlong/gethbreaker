92e4367399aa63e7503d131783a52b1992d0bb29
Hunter Long <info@socialeck.com>
2018-06-17 13:45:29 -0600

    new

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Geth Breaker
+A simple to use tool to break into your Ethereum JSON keystore wallet with a password list.
 
+
+## Installation
+Download the executable built for your operating system in the [GethBreaker Releases](https://github.com/hunterlong/gethbreaker/releases/latest) section.
+Once you have the file downloaded, make it executable.
+
+```
+chmod +x gethbreaker-osx-x64
+mv gethbreaker-osx-x64 /usr/local/bin/gethbreaker
+gethbreaker
+```
+
+## Usage
+```
+gethbreaker wallet.json passwords.txt
+```
--- main.go
+++ main.go
@@ -6,46 +6,80 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"io/ioutil"
 	"os"
+	"sync"
 )
 
 var (
-	passwords []string
+	passwords    []string
+	walletData   []byte
+	walletFile   string
+	passwordFile string
+	guard        chan struct{}
+	passWait     *sync.WaitGroup
 )
 
 func main() {
+	fmt.Println("=============================")
+	fmt.Println("       GETH BREAKER")
+	fmt.Println("=============================")
+	fmt.Println("Usage:")
+	fmt.Println("  gethbreaker wallet.json passwords.txt")
 
-	wallet := "wallet.json"
-	passwordFile := "passwords.txt"
+	if len(os.Args) < 2 {
+		fmt.Println("Missing data! Usage command:")
+		fmt.Println("  gethbreaker wallet.json passwords.txt")
+		os.Exit(1)
+	}
+
+	walletFile = os.Args[1]
+	passwordFile = os.Args[2]
+
+	guard = make(chan struct{}, 8)
+	passWait = new(sync.WaitGroup)
+
+	fmt.Printf("Loading wallet file:    %v\n", walletFile)
+	fmt.Printf("Loading passwords list: %v\n", passwordFile)
+	fmt.Println("=============================")
 
 	file, err := os.Open(passwordFile)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Could not open file: %v\n", passwordFile)
+		os.Exit(1)
 	}
 
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+		passWait.Add(1)
 		passwords = append(passwords, scanner.Text())
 	}
 
-	data, err := ioutil.ReadFile(wallet)
+	walletData, err = ioutil.ReadFile(walletFile)
 	if err != nil {
 		panic(err)
 	}
 
-	for k, pass := range passwords {
-
-		fmt.Printf("%v | trying: %v\n", k, pass)
-
-		key, err := keystore.DecryptKey(data, pass)
-		if err != nil {
-
-		} else {
-			fmt.Printf("\nFound password for %v!\nAddress: %v\nPassword: %v\n", wallet, key.Address.String(), pass)
-			os.Exit(1)
-		}
-
+	for _, v := range passwords {
+		guard <- struct{}{}
+		pass := v
+		go TryPassword(pass)
 	}
 
+	passWait.Wait()
+	fmt.Println("Process is complete")
+
+}
+
+func TryPassword(password string) {
+	fmt.Printf("Trying: %v\n", password)
+	key, err := keystore.DecryptKey(walletData, password)
+	if err == nil {
+		fmt.Printf("\nFound password!!!\n")
+		fmt.Printf("File:     %v\n", walletFile)
+		fmt.Printf("Address:  %v\n", key.Address.String())
+		fmt.Printf("Password: %v\n", password)
+		os.Exit(1)
+	}
+	<-guard
 }
